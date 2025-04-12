f8ca5493a9b38019f5d62b699f172cca1983caa8
K-kare <kamunawangari@gmail.com>
2025-04-12 20:53:20 +0300

    Initial Commit

---
+++ musker/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path('', views.home, name="home" ),
+    path('profile_list/', views.profile_list, name="profile_list" ),
+    path('profile/<int:pk>', views.profile, name="profile"),
+    path('profile/follower/<int:pk>', views.followers, name="followers"),
+    path('profile/follows/<int:pk>', views.follows, name="follows"),
+    path('login/', views.login_user, name="login"),
+    path('logout', views.logout_user, name="logout"),
+    path('register/', views.register_user, name="register"),
+    path('update_user/', views.update_user, name="update_user"),
+    path('meep_like/<int:pk>', views.meep_like, name="meep_like"),
+    path('show_meep/<int:pk>/', views.show_meep, name='show_meep'),
+    path('unfollow/<int:pk>/', views.unfollow, name='unfollow'),
+    path('follow/<int:pk>/', views.follow, name='follow'),
+    path('delete/<int:pk>/', views.delete_meep, name='delete_meep'),
+    path('search/', views.search, name='search'),
+    path('search_user/', views.search_user, name='search_user'),
+]
